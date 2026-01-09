28a0c699094bceb0bed7e055bd4b0f2c30961200
Felipe Martin <FelipeTheFeh@gmail.com>
2026-01-08 21:38:47 -0300

    Corrigir erro de digitação na configuração da base do Vite

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Felipe-Portifólio/',
+  base: '/Felipe-Portifolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
