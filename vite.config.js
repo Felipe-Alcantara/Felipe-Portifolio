89e5cb0bf8391e61963c4deebcc7806500041167
Felipe Martin <FelipeTheFeh@gmail.com>
2026-04-12 17:08:49 -0300

    Domínio

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Felipe-Portifolio/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
